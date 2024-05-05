fbef9653c1738e91d8073cc26d7ad5d8c47e6884
Joel Bamfo <joel.bamfo@amalitech.com>
2024-05-05 23:36:23 +0000

    Q1

---
+++ src/main/java/org/example/Bank.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Bank {
+    public static void main(String[] args) {
+        //Question 1
+        int bankBalance = 500;
+        bankBalance = bankBalance + 250;
+        bankBalance = bankBalance - 100;
+        System.out.println("Bank Balance: $" + bankBalance);
+
+        double Fahrenheit = 95;
+        double Celsius = Fahrenheit - (32 * 5 / 9);
+        System.out.println("Fahrenheit: " + Fahrenheit);
+        System.out.println("Conversion to Celsius: " + Celsius);
+
+        //Question 2
+
+
+//        for (int i = 1; i <= 5; i++) {
+//
+//            System.out.println("i = " + i);
+//        }
+    }
+
+}
